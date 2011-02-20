3e1eb9e82ea9fab00c523c680db5cd2ffd79a63d
seungbeomi <seungbeomi@bc4883c6-4e87-11de-8d14-a1ddb34ee23c>
2011-02-20 13:50:43 +0000

    メッセージ機能追加

---
+++ project/leinbow-web-ec-front/src/main/java/com/leinbow/message/MessageId.java
@@ -0,0 +1,8 @@
+package com.leinbow.message;
+
+public class MessageId {
+
+	private static final String BASE_ERROR_ID = "ERROR";
+	public static final String FILE_NOT_FOUND = BASE_ERROR_ID + "0001";
+
+}
